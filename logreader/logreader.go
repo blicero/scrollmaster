213ea5f6e08365dc97a7737da3ac8062791b9203
Benjamin Walkenhorst <krylon@posteo.de>
2024-08-13 21:04:27 +0200

    Add package logreader

---
+++ logreader/logreader.go
@@ -0,0 +1,8 @@
+// /home/krylon/go/src/github.com/blicero/scrollmaster/logreader/logreader.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 13. 08. 2024 by Benjamin Walkenhorst
+// (c) 2024 Benjamin Walkenhorst
+// Time-stamp: <2024-08-13 20:52:57 krylon>
+
+// Package logreader implements the reading/parsing of log files or journald's log.
+package logreader
