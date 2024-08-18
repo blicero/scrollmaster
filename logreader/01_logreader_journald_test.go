f84c24e436f164893b2fa1e78382e466ca19392f
Benjamin Walkenhorst <krylon@posteo.de>
2024-08-18 20:11:49 +0200

    Start testing Logreader

---
+++ logreader/01_logreader_journald_test.go
@@ -0,0 +1,7 @@
+// /home/krylon/go/src/github.com/blicero/scrollmaster/logreader/01_logreader_journald_test.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 18. 08. 2024 by Benjamin Walkenhorst
+// (c) 2024 Benjamin Walkenhorst
+// Time-stamp: <2024-08-18 20:10:37 krylon>
+
+package logreader
