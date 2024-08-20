a553bc7e3fd0a969a9180db1658b06642d09269e
Benjamin Walkenhorst <krylon@posteo.de>
2024-08-20 22:30:19 +0200

    Add server/agent.go

---
+++ server/agent.go
@@ -0,0 +1,9 @@
+// /home/krylon/go/src/github.com/blicero/scrollmaster/server/agent.go
+// -*- mode: go; coding: utf-8; -*-
+// Created on 20. 08. 2024 by Benjamin Walkenhorst
+// (c) 2024 Benjamin Walkenhorst
+// Time-stamp: <2024-08-20 22:30:12 krylon>
+
+package server
+
+// Methods for handling the Agents
