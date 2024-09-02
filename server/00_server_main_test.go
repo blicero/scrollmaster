1094ae8342137061414bbf957f6f4a47e7c01027
Benjamin Walkenhorst <krylon@posteo.de>
2024-09-02 19:59:08 +0200

    Server: Delete test folder unless tests failed

--- server/00_server_main_test.go
+++ server/00_server_main_test.go
@@ -2,7 +2,7 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 05. 06. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-08-30 20:02:43 krylon>
+// Time-stamp: <2024-08-31 18:20:06 krylon>
 
 package server
 
@@ -51,7 +51,7 @@ func TestMain(m *testing.M) {
 		// database inside it) around, so we can manually inspect it
 		// if needed.
 		// If all tests pass, OTOH, we can safely remove the directory.
-		fmt.Printf("NOT Removing BaseDir %s\n",
+		fmt.Printf("Removing BaseDir %s\n",
 			baseDir)
 		_ = os.RemoveAll(baseDir)
 	} else {
