136e94162e687f3209b3cff5472561dbd0202306
Benjamin Walkenhorst <krylon@posteo.de>
2024-09-03 18:20:50 +0200

    Start fleshing out main.go

--- main.go
+++ main.go
@@ -2,14 +2,18 @@
 // -*- mode: go; coding: utf-8; -*-
 // Created on 15. 08. 2024 by Benjamin Walkenhorst
 // (c) 2024 Benjamin Walkenhorst
-// Time-stamp: <2024-08-15 19:18:32 krylon>
+// Time-stamp: <2024-09-03 15:15:00 krylon>
 
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/blicero/scrollmaster/common"
+	"github.com/blicero/scrollmaster/server"
 )
 
 func main() {
@@ -19,4 +23,86 @@ func main() {
 		common.BuildStamp.Format(common.TimestampFormat))
 
 	fmt.Println("IMPLEMENT ME!")
+
+	const defaultAddr = "::1"
+
+	var (
+		err  error
+		addr string
+		mode string
+		port int
+	)
+
+	flag.StringVar(
+		&addr,
+		"address",
+		defaultAddr,
+		"The IP address to either listen on or connect to.")
+	flag.StringVar(
+		&mode,
+		"mode",
+		"",
+		"Are we the server or the agent?")
+	flag.IntVar(
+		&port,
+		"port",
+		common.Port,
+		"The TCP port to listen on or connect to")
+
+	flag.Parse()
+
+	if err = common.InitApp(); err != nil {
+		fmt.Fprintf(
+			os.Stderr,
+			"Cannot initialize directory %s: %s\n",
+			common.BaseDir,
+			err.Error(),
+		)
+		os.Exit(1)
+	}
+
+	addr = fmt.Sprintf("[%s]:%d",
+		addr,
+		port)
+
+	switch strings.ToLower(mode) {
+	case "server":
+		// Be servile
+		runServer(addr, port)
+	case "agent":
+		// Display some agency
+		runAgent(addr, port)
+	default:
+		fmt.Fprintf(
+			os.Stderr,
+			"Invalid mode %q (must be either \"agent\" or \"server\")\n",
+			mode)
+		os.Exit(1)
+	}
 } // func main()
+
+func runServer(addr string, port int) {
+	var (
+		err   error
+		srv   *server.Server
+		laddr string
+	)
+
+	laddr = fmt.Sprintf("[%s]:%d",
+		addr,
+		port)
+
+	if srv, err = server.Create(laddr); err != nil {
+		fmt.Fprintf(
+			os.Stderr,
+			"Failed to create Server: %s\n",
+			err.Error())
+		os.Exit(2)
+	}
+
+	srv.ListenAndServe()
+}
+
+func runAgent(addr string, port int) {
+	fmt.Println("IMPLEMENT ME!")
+}
